9341439f35f0f8b1326e8fc04764faeae88d6132
Mings <kk0704@lnpgames.com>
2026-04-17 16:33:48 +0900

    Debug: log module load and remove channel filter temporarily

--- src/global/actions/apiUpdaters/keywordTracker.ts
+++ src/global/actions/apiUpdaters/keywordTracker.ts
@@ -2,24 +2,25 @@ import { addActionHandler } from '../../index';
 
 import { pushMessageText } from '../../../util/keywordTracker';
 
+// eslint-disable-next-line no-console
+console.log('[trending] action handler module loaded');
+
 addActionHandler('apiUpdate', (global, actions, update) => {
   if (update['@type'] !== 'newMessage') return undefined;
 
   const message = update.message;
-  if (!message) return undefined;
-
   const chat = global.chats.byId[update.chatId];
-  const text = message.content?.text?.text;
+  const text = message?.content?.text?.text;
 
   // eslint-disable-next-line no-console
   console.log('[trending] newMessage', {
     chatId: update.chatId,
     isChannel: chat?.isChannel,
+    isBroadcast: chat?.type,
     hasText: Boolean(text),
     textPreview: text?.slice(0, 40),
   });
 
-  if (!chat?.isChannel) return undefined;
   if (!text) return undefined;
 
   const timestamp = message.date ? message.date * 1000 : Date.now();
